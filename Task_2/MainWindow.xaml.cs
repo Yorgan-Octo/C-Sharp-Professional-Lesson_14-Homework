69200d5b63e19fcfb47fd10830b1dd55caf3b433
Artem Priadchenko <yorgan.info@gmail.com>
2023-07-13 15:30:08 +0300

    Добавьте файлы проекта.

---
+++ Task_2/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task_2
+{
+    public partial class MainWindow : Window
+    {
+
+
+        System.Timers.Timer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ConnectionDataButton.IsEnabled = true;
+            DisconnectDataButton.IsEnabled = false;
+
+            timer = new System.Timers.Timer();
+
+            timer.Interval = 4000;
+            timer.Elapsed += Tic_Timer;
+
+        }
+
+
+        async void Tic_Timer(object sender, ElapsedEventArgs e)
+        {
+            string text = await Task.Run(() =>
+            {
+                return "Дані отримані\n";
+            });
+
+            Dispatcher.Invoke(() =>
+            {
+                InfoBox.Text += text;
+                InfoBox.ScrollToEnd();
+                timer.Stop();
+                DisconnectDataButton.IsEnabled = !DisconnectDataButton.IsEnabled;
+                ConnectionDataButton.IsEnabled = !ConnectionDataButton.IsEnabled;
+            });
+
+
+        }
+
+
+
+
+        private async void ConnectionDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            InfoBox.Text += await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                timer.Start();
+                return "Підключення До БД\n";
+            });
+
+        }
+
+        private async void DisconnectDataButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            InfoBox.Text += await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                timer.Start();
+                return "Відключення від БД\n";
+            });
+
+
+        }
+    }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public struct FileNames
+    {
+        public string NameFilIn { get; set; }
+        public string NameFilOu { get; set; }
+
+        public FileNames(string nameFilIn, string nameFilOu)
+        {
+            NameFilIn = nameFilIn;
+            NameFilOu = nameFilOu;
+        }
+
+    }
+
+
+    internal class Program
+    {
+
+        public static object ob = new object();
+
+        public static void CotiFile(object fileNames)
+        {
+            FileNames name = (FileNames)fileNames;
+
+            using StreamReader reader = new StreamReader(name.NameFilIn);
+            string temp = reader.ReadToEnd();
+            Console.WriteLine(temp);
+
+
+            Monitor.Enter(ob);
+
+
+            using StreamWriter writer = File.AppendText(name.NameFilOu);
+            writer.WriteLine(temp);
+
+
+            Monitor.Exit(ob);
+        }
+
+        public static async void CotiFileAsync(object fileNames)
+        {
+            await Task.Factory.StartNew(CotiFile, fileNames);
+        }
+
+
+
+        static void Main()
+        {
+
+            FileNames fileNames1 = new FileNames("FilIn1.txt", "Res.txt");
+            FileNames fileNames2 = new FileNames("FilIn2.txt", "Res.txt");
+
+            CotiFileAsync(fileNames1);
+            CotiFileAsync(fileNames2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
